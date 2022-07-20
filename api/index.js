6be6d35515075278bc5664faadc56e105149d1a7
Alexander Mangel <cygnusfear@gmail.com>
2022-07-20 18:03:46 +0100

    stringify

--- api/index.js
+++ api/index.js
@@ -2,10 +2,11 @@ import Parser from "rss-parser";
 let parser = new Parser();
 
 export default async function run(req, res) {
+  console.log(req);
   let query = req.query;
   if (!query.url) {
     res.status(400).end();
   }
   let feed = await parser.parseURL(query.url);
-  res.end(feed);
+  res.end(JSON.stringify(feed));
 }
