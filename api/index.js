307cf6ec23d897549ed6d9c6ea46f4915a4b1574
Alexander Mangel <cygnusfear@gmail.com>
2022-07-20 17:59:59 +0100

    debugger

--- api/index.js
+++ api/index.js
@@ -1,11 +1,11 @@
 import Parser from "rss-parser";
 let parser = new Parser();
 
-export default run = async (req, res) => {
+export default async function run(req, res) {
   let query = req.query;
   if (!query.url) {
     res.status(400).end();
   }
   let feed = await parser.parseURL(query.url);
   res.end(feed);
-};
+}
