f1153033d1dbadfb5d80ef21e3e7844855fddfdf
Alexander Mangel <cygnusfear@gmail.com>
2022-07-23 12:38:04 +0100

    short time

--- src/components/ArticleSource.jsx
+++ src/components/ArticleSource.jsx
@@ -1,8 +1,12 @@
 import TimeAgo from "react-timeago";
+import shortStrings from "react-timeago/lib/language-strings/en-short";
+import buildFormatter from "react-timeago/lib/formatters/buildFormatter";
+
+const formatter = buildFormatter(shortStrings);
 
 export const ItemSource = ({ article }) => {
   return (
-    <div className="mb-3 text-xs font-normal text-stone-700 break-inside-avoid ">
+    <div className="mb-3 mt-2 md:mt-1 text-xs font-normal text-stone-700 break-inside-avoid ">
       <div
         className="mr-2"
         style={{
@@ -18,7 +22,7 @@ export const ItemSource = ({ article }) => {
       {article.siteName}{" "}
       <span className="text-yellow-800 font-extrabold">&#183;</span>
       &nbsp;
-      <TimeAgo date={article.isoDate} />
+      <TimeAgo date={article.isoDate} formatter={formatter} />
     </div>
   );
 };
